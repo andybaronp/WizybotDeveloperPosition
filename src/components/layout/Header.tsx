0ebe2c12f099ef72abe0e843b10faa370b8fe742
Jhoandris B <jrbaronp@gmail.com>
2025-05-07 09:53:34 -0500

    Wrapped the logo in a link

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,9 @@ const Header = () => {
   return (
     <header className='container mx-auto flex justify-between items-center p-4   shadow-sm'>
       <div className='flex-shrink-0'>
-        <img src={logo} alt='WizyBot' className='h-10' />
+        <a href='/'>
+          <img src={logo} alt='WizyBot' className='h-10' />
+        </a>
       </div>
       <nav className='hidden sm:block bg-gradient-to-b from-gray-900 to-stone-950 text-white  px-4 py-2 rounded-2xl'>
         <div className='max-w-7xl mx-auto  '>
