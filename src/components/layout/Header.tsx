2bbbcae12f8035f314669ad34c67d8d62a312bfd
Jhoandris B <jrbaronp@gmail.com>
2025-05-07 10:03:50 -0500

    Wrapped the logo in a link

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
         </a>
       </div>
       <nav className='hidden sm:block bg-gradient-to-b from-gray-900 to-stone-950 text-white  px-4 py-2 rounded-2xl'>
-        <div className='max-w-7xl mx-auto  '>
+        <div className='max-w-xl mx-auto  '>
           <div className='flex gap-6 text-sm font-medium'>
             <a href='#'>Inicio</a>
             <a href='#'>Chatbot</a>
