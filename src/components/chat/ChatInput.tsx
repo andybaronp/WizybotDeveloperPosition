382eb2d2b1e69128c8fd9c6d91f33632e8df7c35
Jhoandris B <jrbaronp@gmail.com>
2025-05-08 08:28:27 -0500

    Fixed input chat

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -17,24 +17,25 @@ const ChatInput = ({ onSend }: { onSend: (text: string) => void }) => {
   };
 
   return (
-    <div className="p-3 border-t border-gray-300">
+    <div className=" border-t border-gray-300">
       <form
-        className="flex items-center bg-white rounded-full border border-gray-300 px-4 py-2"
+        className="flex items-center justify-center bg-gray-100 px-2 py-2"
         onSubmit={handleSubmit}
       >
-        <span className="text-gray-400">
-          <div className="w-5 h-5 text-gray-400">😊</div>
-        </span>
+        <div className="size-6 text-gray-400">😊</div>
+
         <input
           type="text"
           placeholder="Write a message..."
-          className="flex-1 outline-none bg-transparent text-sm mx-2"
+          className="flex-1 outline-none h-6 text-xs mx-2 bg-transparent"
           value={message}
           onChange={onMessageChange}
         />
-        <div className="text-xs mr-2 text-gray-400">{message.length}/140</div>
+        <div className="text-[10px] mr-2 text-gray-400">
+          {message.length}/140
+        </div>
         <button
-          className="bg-blue-950 text-white rounded-full p-2 cursor-pointer "
+          className="bg-blue-950 text-white rounded-full p-1 cursor-pointer "
           type="submit"
         >
           <Send className="w-3 h-3" />
