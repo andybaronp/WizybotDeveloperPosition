8d7c4c15f17eee715013de5a376e491d91074a71
Jhoandris B <jrbaronp@gmail.com>
2025-05-08 11:07:40 -0500

    Fixed PRODUCT_RECOMMENDATION_TRIGGER

--- src/utils/constans.ts
+++ src/utils/constans.ts
@@ -1,5 +1,5 @@
 export const CONSTANTS = {
-  PRODUCT_RECOMMENDATION_TRIGGER: "hola",
+  PRODUCT_RECOMMENDATION_TRIGGER: "I want product recommendations",
   DEFAULT_GREETING_MESSAGE: "Hello there! Do you need any help?",
   API_QUOTES: "https://dummyjson.com/quotes/random",
   API_PRODUCTS: "https://api.wizybot.com/products/demo-product-list",
