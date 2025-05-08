6843454c4c2a3abf3535e36a8e8c4e3147c5b3e6
Jhoandris B <jrbaronp@gmail.com>
2025-05-07 21:39:06 -0500

    add : link to page

--- src/components/chat/ChatFooter.tsx
+++ src/components/chat/ChatFooter.tsx
@@ -1,8 +1,15 @@
 import wizybot from "../../assets/wizybot.webp";
 const ChatFooter = () => {
   return (
-    <div className="py-0.5 px-4 text-center text-xs text-gray-400 flex items-center justify-center bg-gray-100 gap-1">
-      Powered by Wizybot
+    <div className="py-0.5 px-4 text-center  text-gray-400 flex items-center justify-center bg-gray-100 gap-1">
+      <a
+        href="https://wizybot.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="underline text-xs"
+      >
+        Powered by Wizybot
+      </a>
       <img src={wizybot} alt="Wizybot" className="w-4 h-4 rounded-full" />
     </div>
   );
