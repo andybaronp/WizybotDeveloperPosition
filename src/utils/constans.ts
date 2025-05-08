b2d2e1d7481491abfad78c47c8269c1ed50dc9d0
Jhoandris B <jrbaronp@gmail.com>
2025-05-08 10:39:00 -0500

    add constants

--- src/utils/constans.ts
+++ src/utils/constans.ts
@@ -1,5 +1,6 @@
 export const CONSTANTS = {
-  // PRODUCT_RECOMMENDATION_TRIGGER: "I want product recommendations",
   PRODUCT_RECOMMENDATION_TRIGGER: "hola",
   DEFAULT_GREETING_MESSAGE: "Hello there! Do you need any help?",
+  API_QUOTES: "https://dummyjson.com/quotes/random",
+  API_PRODUCTS: "https://api.wizybot.com/products/demo-product-list",
 };
